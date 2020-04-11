fcd43ecd7e8cf2516b91b9c9ea7284bc0aafabb6
azanbinzahid <azanbinzahid@gmail.com>
2020-04-11 18:07:53 +0500

    improve code and proper CLI commands

--- fwd_msgs.py
+++ fwd_msgs.py
@@ -1,16 +1,20 @@
 from slacker import Slacker
+# pip install slacker
+
 from config import slackAPItoken
+# OR
+# slackAPItoken = "YOUR-LEGACY-API-TOKEN-HERE"
 
 
 # Authenticate with slacker
-# get from Slack legacy API
 slack = Slacker(slackAPItoken)
 
 if slack.api.test().successful:
     print(
-        f"Connected to {slack.team.info().body['team']['name']}.")
+        f"Connected to {slack.team.info().body['team']['name']}.\n")
 else:
     print('Try Again!')
+    exit()
 
 
 # Make a request to get the channels
@@ -21,28 +25,52 @@ channels = r.body
 for i, c in enumerate(channels['channels']):
     print(f'{i}. Channel {c["name"]} ')
     # print(f'Channel {c["name"]} ID {c["id"]} Purpose: {c["purpose"]["value"]}')
+print("")
 
+# FROM
+ch = int(input("Select **FROM** channel from list: "))
+fromChannel = channels['channels'][ch]
+print("You selected channel {}\n".format(fromChannel['name'] ))
 
+# TO
+ch = int(input("Select **TO** channel from list: "))
+toChannel = channels['channels'][ch]
+print("You selected channel {}\n".format(toChannel['name'] ))
 
-ch = int(input("\nSelect channel from list: "))
-fromChannel = channels['channels'][ch]['id']
-
-# hardcoded for testing purpose
-toChannel = "G010XT1138X"
+# hardcoded for testing purpose, comment this to use **TO** toChannel instead 
+toChannel['id'] = "G010XT1138X"
 
 # Make a request to get conversation history
-r = slack.conversations.history(fromChannel)
+r = slack.conversations.history(fromChannel['id'])
 conversations = r.body
 
-# Iterate through conversation body
-for msg in conversations["messages"]:
+
+sender_name = input("Include sender name in forward message? (Y/N): ")
+yes = ['Y', 'y', 'yes', 'Yes']
+success = []
+
+# loop thorugh all msgs and send them one-by-one
+for msg in conversations["messages"][::-1]:
+    if (sender_name in yes ):
+        user = "<@" +msg['user'] + ">: "
+    else:
+        user = ""
+    
+    # exclude group join msgs
+    if('subtype' in msg and msg['subtype']=='group_join'):
+        continue
+
+
     if "attachments" in msg:
-        r = slack.chat.post_message(channel=toChannel, text=msg['text'], attachments= msg['attachments'])
+        r = slack.chat.post_message(channel=toChannel['id'], text=user + msg['text'], attachments= msg['attachments'])
     elif "files" in msg:
         continue
-        # r = slack.chat.post_message(channel="G010XT1138X", text=msg['text'], attachments= msg['files'])
     else:
-        r = slack.chat.post_message(channel="G010XT1138X", text=msg['text'])
+        r = slack.chat.post_message(channel=toChannel['id'], text=user + msg['text'])
 
-    # response = r.body
-    # print(response['ok'])
+    response = r.body
+    success.append(response['ok'])
+
+print("")
+print("{} messages succesfully forwarded".format(success.count(True)))
+print("{} messages failed while forwarding".format(success.count(False)))
